48f8933ceca588bc28d0a361bf701bc0346a21d1
maxaguirre5 <maxaguirre5@gmail.com>
2024-06-02 18:44:06 -0700

    add instrumentation & set key env

--- src/frontend/utils/telemetry/HoneycombFrontendTracer.ts
+++ src/frontend/utils/telemetry/HoneycombFrontendTracer.ts
@@ -2,7 +2,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
-import { HoneycombWebSDK } from '@honeycombio/opentelemetry-web';
+import { HoneycombWebSDK, WebVitalsInstrumentation } from '@honeycombio/opentelemetry-web';
+// ^ on github we use this, in the docs we omit WebVitals and do it piecemeal. IMO this should be explained if they're going to stay different.
 import {SessionIdProcessor} from "./SessionIdProcessor";
 
 
@@ -12,9 +13,11 @@ const { NEXT_PUBLIC_OTEL_SERVICE_NAME = '', NEXT_PUBLIC_OTEL_EXPORTER_OTLP_TRACE
 const FrontendTracer = async () => {
   const sdk = new HoneycombWebSDK({
     skipOptionsValidation: true, // because we are not including apiKey
+                                 // but WHY aren't we? It's because we're not direct sending to api, we're using the collector
     serviceName: NEXT_PUBLIC_OTEL_SERVICE_NAME,
     spanProcessor: new SessionIdProcessor(),
     endpoint: NEXT_PUBLIC_OTEL_EXPORTER_OTLP_TRACES_ENDPOINT || 'http://localhost:4318/v1/traces',
+    // this will be updated to the CA collector once instrumentation is expanded and this is moved to k8s
     instrumentations: [getWebAutoInstrumentations({
       '@opentelemetry/instrumentation-fetch': {
         propagateTraceHeaderCorsUrls: /.*/,
@@ -23,7 +26,8 @@ const FrontendTracer = async () => {
           span.setAttribute('app.synthetic_request', IS_SYNTHETIC_REQUEST);
         },
       },
-    })], // add automatic instrumentation
+    }), new WebVitalsInstrumentation()], // add automatic instrumentation
+                                         // ^ update docs to explain what this means, it's supposed to mean that more instrumentation (like webvitals) should be added there, but it isn't clear imo.
   });
   sdk.start();
 };
